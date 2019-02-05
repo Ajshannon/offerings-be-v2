2a4953b8543cec2bc418e2a07029f7e0d6712073
Anthony shannon <anthonyshannon@Jacobs-MBP-3.localdomain>
2019-02-05 10:27:54 -0500

    relations have been fixed, username and user objects now persist through all models.

--- offerings_be/offerings/models.py
+++ offerings_be/offerings/models.py
@@ -12,3 +12,4 @@ class Offerings(models.Model):
     image = models.URLField(max_length=200)
     date = models.DateTimeField(auto_now=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None)
+
--- offerings_be/profile/models.py
+++ offerings_be/profile/models.py
@@ -6,6 +6,6 @@ class Profile(models.Model):
     user = models.OneToOneField("users.User", on_delete=models.CASCADE, null=True, blank=True)
     username = models.CharField(max_length=50, default="username", null=True, blank=True)
 
-    # def __str__(self):
-    #     return self.user.username
+    def __str__(self):
+        return self.username
  
