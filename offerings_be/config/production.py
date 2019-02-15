1cc11bb454b37a96be7707f84f474b51c3f3e4a5
Matt Parsons <maparsons83@yahoo.com>
2019-02-15 10:29:41 -0500

    updated production settings for ec2 integration.

--- offerings_be/config/production.py
+++ offerings_be/config/production.py
@@ -1,5 +1,6 @@
 import os
 from .common import Common
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Production(Common):
@@ -7,26 +8,25 @@ class Production(Common):
     SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
     # Site
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ["52.14.135.120"]
     INSTALLED_APPS += ("gunicorn", )
+    STATIC_ROOT = "/home/ec2-user/offerings-be-v2/static"
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/2.0/howto/static-files/
-    # http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += ('storages',)
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
-    MEDIA_URL = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
+        # Postgres
+    DATABASES = {
+	    'default': {
+    		'ENGINE': 'django.db.backends.postgresql',
+    		'NAME': 'offerings',
+    		'USER': 'offerings',
+    		'PASSWORD': 'LetsDoThis2018',
+    		'HOST': 'offerings.ccdjwdmacumw.us-east-2.rds.amazonaws.com',
+    		'PORT': '5432',
+        }
+    }
 
-    # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
-    # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
-    # 86400 = (60 seconds x 60 minutes x 24 hours)
+    DEBUG = True
+    
     AWS_HEADERS = {
         'Cache-Control': 'max-age=86400, s-maxage=86400, must-revalidate',
     }
