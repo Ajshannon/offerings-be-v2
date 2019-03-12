f214a140a96955bd1043ff8dd0b7f806dd0ca529
Anthony shannon <anthonyshannon@Jacobs-MBP-3.localdomain>
2019-03-12 15:04:44 -0400

    added fields for profile

--- offerings_be/offerings/serializers.py
+++ offerings_be/offerings/serializers.py
@@ -6,4 +6,4 @@ class OfferingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Offerings
         fields = ('id', 'title', 'description', 'address', 'image', 'profile', 'date')
-        read_only_fields = ('id', 'date', )
+        read_only_fields = ('id', 'date')
